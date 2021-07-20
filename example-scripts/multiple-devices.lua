0591cf56a81b4febcbd23a5f84cd81a4ee022da1
Joe Riemersma <joe@iotechsys.com>
2021-07-20 10:05:58 +0100

    DSR-1955: Updated multiple-devices example script
    Signed-off-by: Joe Riemersma <joe@iotechsys.com>

--- example-scripts/multiple-devices.lua
+++ example-scripts/multiple-devices.lua
@@ -3,8 +3,9 @@ TST_SRVC1 = "0000180d-0000-1000-8000-00805f9b34fb"
 TST_CHR1 = "00002a38-0000-1000-8000-00805f9b34fb"
 TST_DESC1 = "12345678-1234-5678-1234-56789abcdef2"
 
+device_count = 5
 
-for i=0, 25, 1
+for i=0, device_count, 1
 do
   dev = ble.createDevice ("lua-test-dev-" .. tostring(i))
   srvc = ble.createService (TST_SRVC1)
